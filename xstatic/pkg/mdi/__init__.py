29bc27f557d366382b172bb07190a3d040a04065
Rob Cresswell <robert.cresswell@outlook.com>
2016-07-14 09:54:32 -0700

    Update XStatic-MDI to 1.6.50.1
    Change-Id: I39a0644c51b231daa4e6e13d29ce5f90893ba40f

--- xstatic/pkg/mdi/__init__.py
+++ xstatic/pkg/mdi/__init__.py
@@ -17,7 +17,7 @@ NAME = __name__.split('.')[-1] # package name (e.g. 'foo' or 'foo_bar')
 
 VERSION = '1.6.50' # version of the packaged files, please use the upstream
                   # version number
-BUILD = '0' # our package build number, so we can release new builds
+BUILD = '1' # our package build number, so we can release new builds
              # with fixes for xstatic stuff.
 PACKAGE_VERSION = VERSION + '.' + BUILD # version used for PyPi
 
